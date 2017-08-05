51919ceb46af0d524448036123a5449e3cf5691b
jscarsbrook <jscarsbrook@gmail.com>
2017-08-06 11:30:33 +1200

    Added implementation concept

--- src/QuestGenerator.ts
+++ src/QuestGenerator.ts
@@ -14,6 +14,30 @@
    limitations under the License.
 */
 
+/*
+    The basic unit for determining quest difficulty is a swing factor.
+    A quest with +10 swing has a large scale positive outcome and a quest
+    with -10 swing has a large scale negative outcome. All quests can have
+    their effects revoked by a equal or larger response.
+
+    Quests are layed out as a series of steps each representing 1 gameplay
+    action. To simplify generation there is only 1 "quest" over the entire
+    game world which determines the structure of the world as a whole. This
+    main quest can diverge into multiple branching parts.
+
+    To add variability to the quests produced some the player has a limited
+    viewpoint on the current quest and all of the factors involved. By
+    restricting a player's knowledge of the internal state multiple side
+    quests can be made with complex interconnecting elements without this
+    nature being immediately apparent.
+
+    The generation is implemented progressively so player steps can influence
+    further development. At a low level a grammar is implemented which
+    determines the order of elements. The concept of this grammar is based off
+    https://dl.acm.org/citation.cfm?id=2000920. This grammar is executed as a
+    state machine.
+*/
+
 function main(args: string[]): number {
     // tslint:disable-next-line:no-console
     console.log("Hello, World");
